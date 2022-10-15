65559c9abf2f7b6b9f3d3cfedf8f582bb7a3d5cb
Patrick Glendon McCullough <p@mccullo.ug>
2022-10-15 13:55:26 -0400

    Pre scrap 2

--- shifty-client/src/App.tsx
+++ shifty-client/src/App.tsx
@@ -4,70 +4,26 @@ import { checkOutcome, checkWord, getWord } from './tools';
 import { WordBlock } from './components/WordBlock/WordBlock';
 
 function App() {
+
   const randomWord = getWord();
   const solutions = checkOutcome(randomWord,0,true);
 
-  const [word, setWord] = useState(randomWord);
-  const [outcomes, setOutcomes] = useState(solutions);
-  const [turn, setTurn] = useState([1,[randomWord,null,null,null,null]]);
-
-  useEffect(() => {
-    if(!outcomes.length) {
-      const randomWord = getWord();
-      console.log("LOAD1"+randomWord);
-      const solutions = checkOutcome(randomWord,0,true);
-      console.log("LOAD2"+solutions);
-      setWord(randomWord);
-      setOutcomes(solutions);
-      setTurn([1,[randomWord,null,null,null,null]]);
-    }
-    console.log(checkWord("DOGS",["DOGS","LOGS"],1));
-  },[outcomes])
+  const [answers,setAnswers] = useState([randomWord]);
+  const [liveWord, setLiveWord] = useState(randomWord);
 
   return (
     <div className="App">
-      {turn[1].map((wordBlock,i) => 
-        i===turn[0]
-        ?
-          <WordBlock
-            key={i}
-            active={true}
-            turnPosition={i}
-            word={turn[1][i-1]}
-            setTurn={setTurn}
-            turn={turn}
-            outcomes={outcomes}
-          />
-        :turn[1][i]
-          ?
-            <WordBlock 
-              key={i}
-              active={false}
-              turnPosition={i}
-              word={turn[1][i]}
-              setTurn={null}
-              outcomes={null}
-            />
-            :
-              <WordBlock 
-                key={i}
-                active={false}
-                turnPosition={i}
-                word={null}
-                setTurn={null}
-                outcomes={null}
-              />
+      {answers.map((word,i)=>
+        <WordBlock 
+          word={word}
+          liveWord={liveWord}
+          setLiveWord={setLiveWord}
+          outcomes={solutions}
+          answers={answers}
+          setAnswers={setAnswers}
+          turn={i+1} 
+        />
       )}
-
-      <div style={{position:"fixed",left:"0px",top:"0px"}}>
-        <p><b>POSSIBLE OUTCOMES FOR {word} ({outcomes.length}):</b></p>
-        {outcomes.length?
-          outcomes.map(solution =>
-            <p key={solution}>{solution}</p>
-          ):
-          ""
-        }
-      </div>
     </div>
   )
 }
--- shifty-client/src/components/WordBlock/WordBlock.tsx
+++ shifty-client/src/components/WordBlock/WordBlock.tsx
@@ -3,46 +3,36 @@ import { checkWord } from "../../tools";
 
 const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-export const WordBlock = ({active, turnPosition, word, setTurn, turn, outcomes}) => {
-    // console.log(active, turnPosition, word, setTurn, outcomes);
+export const WordBlock = ({liveWord, setLiveWord, answers, setAnswers, word, outcomes, turn}) => {
+  
+
+  if(turn===answers.length) {
+    const charSelect = (event) => {
+      if(alphabet.includes(event.key.toUpperCase())) {
+        const newWord = word.replace("_",event.key.toUpperCase());
+        console.log("I'm running check because "+turn+" equals "+answers.length);
+        console.log("PROOF: ",answers);
+        checkWord(newWord,outcomes,turn,answers,setAnswers);
+      }
+    };
+    
+    useEffect(() => {
+      word = word.split("");
+      word[turn-1] = "_";
+      word = word.join("");
+      setLiveWord(word);
+      document.addEventListener("keydown", charSelect, false);
+      return () => {
+        document.removeEventListener("keydown", charSelect, false);
+      };
+    }, [charSelect]);
+  }
 
     
-    const [stateWord, setStateWord] = useState(word);
-
-    if(word&&active) {
-        const charSelect = useCallback((event) => {
-            if(alphabet.includes(event.key.toUpperCase())) {
-              const newWord = word.replace("_",event.key.toUpperCase());
-              checkWord(newWord,outcomes,turnPosition);
-            }
-          }, []);
-        
-          useEffect(() => {
-            console.log("before split?",word);
-            word = word.split("");
-            word[turnPosition-1] = "_";
-            word = word.join("");
-            setStateWord(word);
-
-            document.addEventListener("keydown", charSelect, false);
-        
-            return () => {
-              document.removeEventListener("keydown", charSelect, false);
-            };
-          }, []);
-    }
 
     return (
-        <>
-        {
-            word?
-                active
-                    ?
-                    <div style={{color: "green",fontWeight:"bold"}}>{stateWord}</div>
-                    :
-                    <div>{stateWord}</div>
-                :<div>_ _ _ _</div>
-        }
-        </>
+        <div>
+          {turn===answers.length?liveWord:answers[turn]||"_ _ _ _"}
+        </div>
     )
 }
